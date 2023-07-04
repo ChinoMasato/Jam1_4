527d04fc1f427d6acf6d63a5e1c6678fcb136467
rikuto <rikuto.sc@gmail.com>
2023-07-04 09:44:15 +0900

    BOMBUI

--- UI.cpp
+++ UI.cpp
@@ -16,6 +16,7 @@ void drawUI()
 	DrawBox(0, 0, player.hp * 5, 20, GetColor(50, 50, 255), TRUE);
 	DrawBox(0, 0, 150, 20, GetColor(255, 255, 255), FALSE);
 	DrawFormatString(200, 0, GetColor(255, 255, 255), "wasd ñÓàÛÉLÅ[Ç≈ëÄçÏÅ@space z Ç≈éÀåÇ");
+
 	DrawBox(0,980,120,999, GetColor(0, 0, 255), 1);
 	DrawBox(1, 980, 120 * (bomb.cooltime / 1000), 999, GetColor(0, 0, 0), 1);
 	DrawBox(0, 980, 120, 1000, GetColor(255, 255, 255), 0);
